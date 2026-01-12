3cc3cd9d02d4f10fe59e41991f75facb58a50961
Aaddityaaa <adityau5090@gmail.com>
2026-01-13 02:38:28 +0530

    both at beginning and end insertion

---
+++ linkedList/Singly_Linked_List/03_bothInsertion.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data, Node* next = nullptr) {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+
+public:
+    SinglyLinkedList() {
+        head = nullptr;
+    }
+
+    void insertionAtEnd(int val) {
+        Node* temp = new Node(val);
+
+        if (head == nullptr) {
+            head = temp;
+            return;
+        }
+
+        Node* t1 = head;
+        while (t1->next != nullptr) {
+            t1 = t1->next;
+        }
+        t1->next = temp;
+    }
+    void insertionAtBeginning(int val) {
+        Node* temp = new Node(val);
+
+        if (head == nullptr) {
+            head = temp;
+            return;
+        }
+
+        temp->next = head;
+        head = temp;
+    }
+
+    void display() {
+        Node* curr = head;
+        while (curr != nullptr) {
+            cout << curr->data << " -> ";
+            curr = curr->next;
+        }
+        cout << "NULL";
+    }
+};
+
+
+int main() {
+    SinglyLinkedList list;
+    list.insertionAtEnd(10);
+    list.insertionAtEnd(20);
+    list.insertionAtEnd(30);
+    list.insertionAtBeginning(5);
+    list.insertionAtBeginning(0);
+
+    list.display();
+    return 0;
+}
