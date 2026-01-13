150ba269f9ec7c1f4d0a0a9cdfea6434261a198b
Aaddityaaa <adityau5090@gmail.com>
2026-01-13 08:53:14 +0530

    insertion after given index

---
+++ linkedList/Singly_Linked_List/05_insertionAtGivenIndex.cpp
@@ -0,0 +1,89 @@
+// Insertion after given index
+
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data, Node* next = nullptr) {
+        this->data = data;
+        this->next = next;
+    }
+};
+
+class SinglyLinkedList {
+private:
+    Node* head;
+
+public:
+    SinglyLinkedList() {
+        head = nullptr;
+    }
+
+    void insertionAtEnd(int val) {
+        Node* temp = new Node(val);
+
+        if (head == nullptr) {
+            head = temp;
+            return;
+        }
+
+        Node* t1 = head;
+        while (t1->next != nullptr) {
+            t1 = t1->next;
+        }
+        t1->next = temp;
+    }
+ 
+    void insertionAfterIndex(int val, int index) {
+    Node* temp = new Node(val);
+
+    if (head == nullptr) {
+        cout << "List is empty\n";
+        delete temp;
+        return;
+    }
+
+    Node* t1 = head;
+    int i = 0;
+
+    // Move to node at given index
+    while (t1 != nullptr && i < index) {
+        t1 = t1->next;
+        i++;
+    }
+
+    if (t1 == nullptr) {
+        cout << "Index " << index << " not found\n";
+        delete temp;
+        return;
+    }
+
+    temp->next = t1->next;
+    t1->next = temp;
+}
+
+    void display() {
+        Node* curr = head;
+        while (curr != nullptr) {
+            cout << curr->data << " -> ";
+            curr = curr->next;
+        }
+        cout << "NULL\n";
+    }
+};
+
+
+int main() {
+    SinglyLinkedList list;
+    list.insertionAtEnd(10);
+    list.insertionAtEnd(20);
+    list.insertionAtEnd(30);
+    list.insertionAfterIndex(60,0);
+    list.insertionAfterIndex(80,2);
+    list.display();
+    return 0;
+}
