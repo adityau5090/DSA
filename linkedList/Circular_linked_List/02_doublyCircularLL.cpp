d266324b67ff633a72ad9bbf703a4b13c6496b47
Aaddityaaa <adityau5090@gmail.com>
2026-01-15 16:21:00 +0530

    add insert at beginning and insert at index method

--- linkedList/Circular_linked_List/02_doublyCircularLL.cpp
+++ linkedList/Circular_linked_List/02_doublyCircularLL.cpp
@@ -42,13 +42,64 @@ class CircularLL{
         head->prev = temp;
     }
 
+    void insertAtBeginning(int val){
+        Node* temp = new Node(val);
+
+        if(head == nullptr){
+            head= temp;
+            head->next = head;
+            head->prev = head;
+            return;
+        }
+
+        temp->next = head;
+        temp->prev = head->prev;
+        head->prev->next = temp;
+        head = temp;
+    }
+
+    void insertAtIndex(int val,int index){
+        Node* temp = new Node(val);
+
+        if(head == nullptr){
+            head = temp;
+            head->next = head;
+            head->prev = head;
+            return;
+        }
+
+        // insert at beginning
+        if(index == 0){
+            temp->next = head;
+            temp->prev = head->prev;
+            head->prev->next = temp;
+            head->prev = temp;
+            head = temp;
+            return;
+        }
+
+        Node* curr = head;
+        int i=0;
+
+        while(curr->next != head && i < index){
+            curr = curr->next;
+            i++;
+        }
+        temp->next = curr;
+        temp->prev = curr->prev;
+        curr->prev->next = temp;
+        curr->prev = temp;
+        
+    }
+    
+    
     void display(){
         Node* curr = head;
         do{
             cout<<curr->data<<" <-> ";
             curr = curr->next;
         }while(curr != head);
-        cout<<"NULL\n";
+        cout<<"(HEAD)\n";
     }
 };
 
@@ -57,5 +108,8 @@ int main(){
     list.insertAtEnd(10);
     list.insertAtEnd(20);
     list.insertAtEnd(30);
+    list.insertAtBeginning(5);
+    list.insertAtIndex(15,2);
+    list.insertAtIndex(25,4);
     list.display();
 }
