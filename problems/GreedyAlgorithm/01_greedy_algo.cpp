5ed85cfa4fabe6e3ac661ad71ff620932f0de830
Aaddityaaa <adityau5090@gmail.com>
2026-02-21 13:05:35 +0530

    greedy algorithm

---
+++ problems/GreedyAlgorithm/01_greedy_algo.cpp
@@ -0,0 +1,32 @@
+                        // Striver's Greedy algorithm Playlist
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int max_greed_Assigned(vector<int> greed, vector<int> cookies){
+    int n = greed.size(); 
+    int m = cookies.size();
+    int i=0,j=0;
+
+    sort(greed.begin(), greed.end());
+    sort(cookies.begin(), cookies.end());
+
+    while(j < m){
+        if(greed[i] <= cookies[j]){
+            i += 1;
+        }
+        j += 1;
+    }
+    
+    return i;
+}
+
+int main(){
+    vector<int> greedy = {1,5,3,3,4};
+    vector<int> cookies = {4,2,1,2,1,3};
+
+    int result = max_greed_Assigned(greedy,cookies);
+    cout<<"Max greed assigned : "<<result;
+}
