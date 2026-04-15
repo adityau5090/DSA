186f008d7f54ede0f3b120c822027e1445c11037
Aaddityaaa <adityau5090@gmail.com>
2026-04-15 23:22:41 +0530

    Morre's voting algorithm

--- myProblems/05_Majority_Element.cpp
+++ myProblems/05_Majority_Element.cpp
@@ -18,11 +18,42 @@ int majorityElement(vector<int> &vec){
     }
 
     return -1;
+
+    // time complexity = O(n) + O(logn)
+    // space complexity = O(n)
+}
+
+// Moore's voting algorithm
+
+int morresAlgo(vector<int> &vec){
+    int count=0;
+    int el;
+    for(int i=0; i<vec.size(); i++){
+        if(count == 0){
+            count = 1;
+            el=vec[i];
+        }else if(vec[i] == el){
+            count++;
+        }else{
+            count--;
+        }
+    }
+    int count1 = 0;
+    for(int i=0; i<vec.size(); i++){
+        if(vec[i] == el) count1++;
+    }
+    if(count1 > (vec.size()/2)){
+        return el;
+    }
+    return -1;
+
+    // time complexity = O(n)
+    // space complexity = O(1)
 }
 
 int main(){
     vector<int> arr = {2,2,1,3,2,1,3,2,2};
-    int ans = majorityElement(arr);
+    int ans = morresAlgo(arr);
     if(ans == -1){
         cout<<"Not found!";
     }else{
