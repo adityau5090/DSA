732fe08fcee259b79d97bd2f990fe493aa9921d9
Aaddityaaa <adityau5090@gmail.com>
2026-04-18 15:15:38 +0530

    another type of rearranging array

--- myProblems/06_Rearrange_Of_Elements.cpp
+++ myProblems/06_Rearrange_Of_Elements.cpp
@@ -25,11 +25,48 @@ void rearrange(vector<int> &vec){
     // Time complexity -> O(n) + O(n)
     // Space complexity -> O(n)   (because we take n/2 space for +ve and n/2 for -ve)
 }
+
+ 
+// second type in this no of -ve elements and no of +ve elements are not same so if any +ve or -ve number left then we have to add them at last without altering its order
+ 
+void rearrange2(vector<int> &vec){
+    vector<int> pos, neg;
+
+    for(int i=0; i<vec.size(); i++){
+        if(vec[i] < 0) neg.push_back(vec[i]);
+        else pos.push_back(vec[i]);
+    }
+
+    if(pos.size() > neg.size()){
+        for(int i=0; i<neg.size(); i++){
+            vec[2*i] = pos[i];
+            vec[2*i+1] = neg[i];
+        }
+        int index = neg.size() * 2;
+        // start loop from remaining +ve elements in +ve array
+        for(int i=neg.size(); i<pos.size(); i++){
+            vec[index] = pos[i];
+            index++;
+        }
+    }else{
+        for(int i=0; i<pos.size(); i++){
+            vec[2*i] = pos[i];
+            vec[2*i+1] = neg[i];
+        }
+        int index = pos.size() * 2;
+        for(int i=pos.size(); i<neg.size(); i++){
+            vec[index] = neg[i];
+            index++;
+        }
+    }
+} 
 int main(){
     vector<int> arr = {3,1,-2,-5,2,-4};
-    rearrange(arr);
+    vector<int> arr2 = {1,2,-4,-5,3,6};
+    // rearrange(arr);
+    rearrange2(arr2);
     cout<<"Rearranged array: ";
-    for(int i: arr){
+    for(int i: arr2){
         cout<<i<<" ";
     }
 return 0;
