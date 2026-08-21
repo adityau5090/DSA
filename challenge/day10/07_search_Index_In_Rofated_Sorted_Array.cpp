e26c716e4c2b60791e58801bfec7a2f9ef8504bf
Aaddityaaa <adityau5090@gmail.com>
2026-08-21 13:37:10 +0530

    search index in reoated sorted array

---
+++ challenge/day10/07_search_Index_In_Rofated_Sorted_Array.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int search(vector<int> vec, int target)
+{
+    int low = 0, high = vec.size() - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (vec[mid] == target)
+        {
+            return mid;
+        }
+        // left half sorted
+        else if (vec[low] <= vec[mid])
+        {
+            if (vec[low] <= target && target <= vec[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        else
+        {
+            if (vec[mid + 1] <= target && target <= vec[high])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+int main()
+{
+    vector<int> vec = {10, 13, 15, 2, 5, 7, 9};
+    cout << "Index : " << search(vec, 5);
+    return 0;
+}
