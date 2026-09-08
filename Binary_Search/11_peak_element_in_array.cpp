c0933e624d5d381b782f15be9a9478b7d9701a36
Aditya <adityau5090@gmail.com>
2026-09-08 13:40:03 +0530

    Implement peak element logic in array
    Implement a function to find the peak element in an array using binary search.

---
+++ Binary_Search/11_peak_element_in_array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int peakElement(vector<int> &arr) {
+    int n = arr.size();
+    if (n == 1)
+        return arr[0];
+
+    if (arr[0] > arr[1])
+        return arr[0];
+
+    if (arr[n - 1] > arr[n - 2])
+        return arr[n - 1];
+
+    int low = 1, high = n - 2;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+            return arr[mid];
+
+        if (arr[mid] > arr[mid-1]) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {1, 2,3,4,5,6,7,8,5,1};
+
+    int ans = peakElement(arr);
+
+    cout << "Peak element is: " << ans << endl;
+
+    return 0;
+}
+```
